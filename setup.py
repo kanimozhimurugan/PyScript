c624bbfffc67c0a92590b03d844da0251479871d
Kanimozhi <mkmozhi1504@gmail.com>
2015-08-22 00:34:45 -0400

    setup for pypassgen

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(name='pypassgen',
+      version=pypassgen.__version__,
+      description='A easy-use module to generate random passwords',
+      long_description=long_description,
+      url='https://github.com/kanimozhimurugan/PyScript/pypassgen',
+      author='kmurugan',
+      author_email='mkmozhi1504@gmail.com',
+      license='Apache License',
+      keywords='pypass randompassword passgen',
+      classifiers=[
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          ],
+        )
